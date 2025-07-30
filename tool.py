ee2e2b67cf78caab321a13e1e9147cba35ccd90d
SwaggyMacro <macro@julym.com>
2025-07-30 10:39:05 +0800

    feat: add Godzi

--- tool.py
+++ tool.py
@@ -4,20 +4,20 @@
 
 import json
 import os
-import requests as req
 
 # the emoji name, will be the key of the new emoji
 # 表情包的名字，将会是新表情包的JSON键
-emoji_name = "TG小黄鸭"
+emoji_name = "小怪兽"
+emoji_short_code = "Godzi"
+
+# the folder where the emoji images are stored
+# 表情包图片存放的文件夹
+folder = "stickers/Godzi"
 
 # the emoji data structure
 # 表情包的数据结构
 emoji_data = f'{{"{emoji_name}": {{ "type": "image", "container": []}}}}'
 emoji_data = json.loads(emoji_data)
-
-# the folder where the emoji images are stored
-# 表情包图片存放的文件夹
-folder = "UtyaDuck"
 
 # the cdn url of the emoji images, end without slash /
 # 表情包JSON的CDN地址，不要以斜杠结尾
@@ -25,11 +25,11 @@ cdn_url = "//raw.githubusercontent.com/SwaggyMacro/OwO_Stickers/main/stickers"
 
 # url of the raw json file
 # JSON文件的URL
-owo_json_url = "https://raw.githubusercontent.com/SwaggyMacro/OwO_Stickers/main/OwO.min.json"
+owo_json_url = "OwO.min.json"
 
 # the property where the new emoji data will be appended after, for example below is append after `TG小黄鸭`
 # 新表情包数据将会被添加到的位置（比如以下是添加到`TG小黄鸭`的后面）
-append_after = "TG小黄鸭"
+append_after = "小黄鸭"
 
 # compress the json file
 # 是否压缩JSON文件
@@ -37,12 +37,14 @@ need_compress = True
 
 # get the json file
 # 获取JSON文件
-owo_json = req.get(owo_json_url).json()
+# owo_json = req.get(owo_json_url).json()
+with open(owo_json_url, "r", encoding="utf-8") as f:
+    owo_json = json.load(f)
 
 for index, file  in enumerate(os.listdir(folder)):
     if file:
         emoji_data[emoji_name]["container"].append(
-            {"text": f"{folder}-{index}", "icon":f"<img src='{cdn_url}/{folder}-Preview/{file}' "
+            {"text": f"{emoji_short_code}-{index}", "icon":f"<img src='{cdn_url}/{folder}-Preview/{file}' "
                                                  f"origin='{cdn_url}/{folder}/{file}'>" ,})
 
 for key, value in owo_json.items():
