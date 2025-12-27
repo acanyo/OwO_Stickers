958329e4c1c8fc042be405292fe32d87892db2fa
SwaggyMacro <macro@julym.com>
2025-12-27 17:13:49 +0800

    feat: add some stickers
    1. SantaEmoji
    2. FaceEmoji
    3. SmilesMAX
    4. MashupEmoji
    5. PepeEmoji
    6. DuckEmoji
    
    fixed PeopleMemes

---
+++ test.py
@@ -0,0 +1,24 @@
+# 删除指定文件夹下所有文件名中的 下划线 _ 以及之前的字符串，如: "file_name.txt" -> "name.txt"
+
+import os
+import re
+
+def remove_emoji_from_filenames(folder_path):
+    # Iterate through all files in the specified folder
+    for filename in os.listdir(folder_path):
+        # Define the new filename by removing everything before and including the first underscore
+        new_filename = re.sub(r'^.*?_', '', filename)
+
+        # Get the full paths for renaming
+        old_file = os.path.join(folder_path, filename)
+        new_file = os.path.join(folder_path, new_filename)
+
+        # Rename the file
+        os.rename(old_file, new_file)
+        print(f'Renamed: "{filename}" to "{new_filename}"')
+
+
+
+# Example usage
+folder_path = r'D:\Development\Project\CSharp\LottieViewConvert\LottieViewConvert\bin\Debug\net8.0\SavedStickers\SantaEmoji\SantaEmoji_output'
+remove_emoji_from_filenames(folder_path)
